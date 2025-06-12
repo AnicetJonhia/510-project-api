65a9b1f11dd7eafa79c4786653f9b5b39cf0baaa
AnicetJonhia <anicet22.aps2a@gmail.com>
2025-06-12 19:47:57 +0300

    auth database seeds users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { SeedModule } from './seeds/seed.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    AuthModule,
+    UsersModule,
+    SeedModule,
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
